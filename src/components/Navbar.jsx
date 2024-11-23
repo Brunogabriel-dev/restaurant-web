b99e15d427b71da7412999745697fbed47c6efab
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-23 11:57:48 -0300

    Inclusão de texto

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
+
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
