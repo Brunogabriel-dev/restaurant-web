16a2251e98f5d3ad4193e9ca71c27e5734c10e3e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-23 14:10:17 -0300

    Inclusão de texto

--- src/components/DishCard.jsx
+++ src/components/DishCard.jsx
@@ -6,8 +6,10 @@ const DishCard = ({ project }) => {
       alt={project.title}
       className="rounded-3xl p-2" />
       <div className="p-4">
-        <h3 className="mb-2 text-2xl font-bold tracking-tighter"></h3>
-
+        <h3 className="mb-2 text-2xl font-bold tracking-tighter">
+          {project.title}
+        </h3>
+        <p className="text-sm">{project.description}</p>
       </div>
     </div>
   )
