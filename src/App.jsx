4f9b97ccb9193ef93f115025f30aafdc7b951511
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-23 11:07:39 -0300

    Inclusão de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline bg-white">
-      restaurante
-    </h1>
+      <HeroSection />
     </div>
   )
 }
