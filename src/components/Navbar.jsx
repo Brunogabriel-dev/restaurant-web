b49cf6c490aafa6027c68369992e0f6b08ced99a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-23 12:08:49 -0300

    Inclusão de texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,27 @@ const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen)
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   }
 
+  const handleScroll = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const offsetTop = targetElement.offsetTop - 80;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: "smooth"
+      })
+    }
+    setIsMobileMenuOpen(false);
+    
+  }
   return (
-    <div>Navbar</div>
+    <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center">
+      <div className="flex w-full items-center justify-between overflow-y-hidden"></div>
+
+    </nav>
   )
 }
 
