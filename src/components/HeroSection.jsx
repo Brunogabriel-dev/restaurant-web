f9f28ec2eca75b5b549409b8e71d0f47cd18a3ca
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-23 11:22:55 -0300

    Inclusão de texto

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,15 @@
+import video from "../assets/hero.mp4"
+import logo from "../assets/logo.png"
+import hero from "../assets/hero.jpeg"
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section className="relative flex h-screen items-center justify-center">
+      <div className="absolute inset-0 -z-20 h-full w-full overflow-hidden">
+        <video 
+        src={video} className="h-full w-full object-cover" muted autoPlay loop playsInline poster={hero}></video>
+      </div>
+      <div className="absolute inset-0 -z-10 bg-gradient-to-b"></div>
+    </section>
   )
 }
 
