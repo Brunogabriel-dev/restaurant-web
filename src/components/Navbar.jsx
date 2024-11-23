ffe1c8bbd31f7dab871197796a4b0f97ece64b26
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-23 12:27:48 -0300

    Inclusão de texto

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import logo from "../assets/logo.png"
+import { LINKS } from "../constants"
 
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -22,8 +24,17 @@ const Navbar = () => {
   }
   return (
     <nav className="fixed top-4 z-50 flex w-full flex-col items-center justify-center">
-      <div className="flex w-full items-center justify-between overflow-y-hidden"></div>
+      <div className="flex w-full items-center justify-between overflow-y-hidden p-4 backdrop-blur-lg lg:m-2 lg:w-[50rem] lg:rounded-full lg:shadow-lg">
+        <img src={logo} alt="logo" width={80} height={22}/>
+        <div className="hidden space-x-6 lg:flex">
+          {LINKS.map((link, index) => (
+            <a key={index} href={`#${link.targetId}`} className={`text-sm ${index !== 0 ? "border-l-2 border-neutral-300/20 pl-2" : ""} hover:opacity-50`} onClick={(e) => handleScroll (e, link.targetId)}>
+              
+            </a>
+          ))}
 
+        </div>
+      </div>
     </nav>
   )
 }
