3fb394268078c43283046345add556bbd7a8112b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-24 19:37:35 -0300

    Inclusão de texto

---
+++ src/components/Review.jsx
@@ -0,0 +1,17 @@
+import { REVIEW } from "../constants"
+
+const Review = () => {
+  return (
+    <section className="container mx-auto mb-8 mt-12" id="review">
+      <div className="flex flex-col">
+        <p className="mb-10 text-2xl font-light leading-normal tracking-tighter lg:mx-40 lg:text-[3.5rem]">
+          {REVIEW.content}
+        </p>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Review
