d703f58fcb1753ff196ebce0335be35bf77ec611
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-24 10:40:53 -0300

    Inclusão de texto

---
+++ src/components/Mission.jsx
@@ -0,0 +1,12 @@
+const Mission = () => {
+  return (
+    <section id="mission">
+      <div className="container mx-auto text-center">
+        <h2 className="mb-8 text-3xl lg:text-4xl"></h2>
+      </div>
+
+    </section>
+  )
+}
+
+export default Mission
