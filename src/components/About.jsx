22dcc74126eca50f1851d3e22a1c6e0dfb29579d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-24 09:23:35 -0300

    Inclusão de texto

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import about from "../assets/about.jpeg"
 const About = () => {
   return (
-    <div>About</div>
+    <section className="container mx-auto mb-8" id="about">
+      <h2 className="mb-8 text-center text-3xl tracking-tighter lg:text-4xl">About Us</h2>
+      <div className="flex flex-wrap">
+        <div className="w-full p-4 lg:w-1/2">
+        <img src={about} className="rounded-3xl lg:-rotate-3" />
+        </div>
+      </div>
+
+    </section>
   )
 }
 
