cbe990f370283b47106fcb5867eeb5fa7c8dcd3e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-24 09:06:57 -0300

    Inclusão de texto

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+import about from "../assets/about.jpeg"
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -10,7 +10,7 @@ const Dishes = () => {
         ))}
       </div>
     </section>
-  )
+  );
 }
 
-export default Dishes
+export default Dishes;
