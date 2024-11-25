2e21214510f062e3ebb5dbaea1e12d6c48d5bdf9
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-25 09:54:41 -0300

    Inclusão de texto

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,7 +1,14 @@
+import { CONTACT } from "../constants"
+
 const ContactSection = () => {
   return (
     <section className="container mx-auto py-16 " id="contact">
       <h2 className="mb-8 text-center text-3xl lg:text-4xl">Contact Us</h2>
+      <div className="text-neutral-400">
+        {CONTACT.map((detail) => (
+          <p key={detail.key}></p>
+        ))}
+      </div>
     </section>
   )
 }
