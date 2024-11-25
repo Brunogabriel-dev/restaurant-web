f59c54693557e9d317117d98a3a35c531f0fdaa1
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-11-25 12:47:28 -0300

    Inclusão de texto

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -7,27 +7,45 @@ import customer4 from "../assets/customer4.jpeg"
 import { motion } from "framer-motion"
 
 const containerVariants = {
-  
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      straggerChildren: 0.8,
+    }
+  }
 }
 
-
+const itemVariants = {
+  hidden: { opacity: 0, y: 20 },
+  show: { opacity: 1, y: 0 , transition: { duration: 0.5 }}
+}
 
 const Review = () => {
   return (
     <section className="container mx-auto mb-8 mt-12" id="review">
-      <div className="flex flex-col">
+      <motion.div 
+       initial="hidden"
+       whileInView="show"
+       variants={containerVariants}
+       viewport={{ once: true }}
+
+      className="flex flex-col">
         <p className="mb-10 text-2xl font-light leading-normal tracking-tighter lg:mx-40 lg:text-[3.5rem]">
           {REVIEW.content}
         </p>
         <div className="flex items-center justify-center gap-6">
-          <img src={xaviour} width={80} alt={REVIEW.name}
+          <img 
+          src={xaviour} 
+          width={80} 
+          alt={REVIEW.name}
           className="rounded-full border" />
           <div className="tracking-tighter">
             <h6>{REVIEW.name}</h6>
             <p className="text-sm text-neutral-500">{REVIEW.profession}</p>
           </div>
         </div>
-      </div>
+      </motion.div>
       <div className="mt-14 flex flex-col items-center justify-center gap-2 md:flex-row">
         {[customer1, customer2, customer3, customer4].map((customer, index) => (
           <img 
